7822e42f856cb9f693afc5ac6fbd0367739f00e0
Richardn <Richardn@163.com>
2021-04-01 13:07:02 +0800

    Use 2050 instead of 80 as http server port
    to remove unnecessary permission requirements. And to avoid possible collision with existing nginx or Apache or whatever service as ws+http-server.js is just for newbies.

--- ws+http-server.js
+++ ws+http-server.js
@@ -6,6 +6,6 @@ const Broadcaster = require('./raspivid-broadcaster');
 const app = express();
 app.use(express.static(__dirname + '/site'));
 const httpServer = http.createServer(app);
-httpServer.listen(80);
+httpServer.listen(2050);
 
 new Broadcaster({width: 640, height: 480, framerate: 20, port: 8080});
